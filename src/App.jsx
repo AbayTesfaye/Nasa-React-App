0889cee5f8aa5f1f0fefbf556861f3576a2e2c3a
AbayTesfaye <abaytesfaye02@gmail.com>
2024-10-08 15:49:20 -0700

    Some Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 import SideBar from "./components/SideBar";
@@ -9,6 +9,26 @@ function App() {
   function handleToggleModal() {
     setShowModal(!showModal);
   }
+
+  useEffect(() => {
+    async function fetchData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+      console.log("NASA API Key:", NASA_KEY); // Should print the actual key
+
+      const url = `https://api.nasa.gov/planetary/apod?api_key=${NASA_KEY}`;
+
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log("DATA\n", data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Main />
